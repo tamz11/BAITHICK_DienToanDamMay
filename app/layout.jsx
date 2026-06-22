0028ae6b8504e91865e7e326a603714a724b99d3
Thuhuong140305 <huongdang2k5@gmail.com>
2026-06-22 19:25:58 +0700

    ho tro khach hang

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
 import { AuthProvider } from "@/app/AuthProvider";
 import ProductFetcher from "@/components/ProductFetcher";
+import Script from "next/script";
 import "./globals.css";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
@@ -21,6 +22,23 @@ export default function RootLayout({ children }) {
                         <ProductFetcher />
                         <Toaster />
                         {children}
+
+                        {/* Zoho SalesIQ Integration - Chat với Staff bằng tiếng Việt */}
+                        <Script id="zoho-salesiq-init" strategy="afterInteractive">
+                            {`
+                                window.$zoho=window.$zoho || {};
+                                $zoho.salesiq=$zoho.salesiq||{
+                                    ready:function(){
+                                        $zoho.salesiq.language("vi");
+                                    }
+                                }
+                            `}
+                        </Script>
+                        <Script
+                            id="zsiqscript"
+                            src="https://salesiq.zohopublic.com/widget?wc=siq2d52c4f445a65aa78b63628be4aaab90"
+                            strategy="afterInteractive"
+                        />
                     </StoreProvider>
                 </AuthProvider>
             </body>
